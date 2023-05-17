48b2cf3a1835443d411ff67ff2714c60a6832477
Marno van der Maas <mvdmaas+git@lowrisc.org>
2023-05-17 17:30:09 +0100

    [i2c,dv] Draft implementation of I2C FSM waiver
    Signed-off-by: Marno van der Maas <mvdmaas+git@lowrisc.org>

---
+++ hw/ip/i2c/dv/cov/i2c_cov_excl.el
@@ -0,0 +1,8 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+ANNOTATION: "[UNSUPPORTED] Multi-host I2C"
+INSTANCE: tb.dut.i2c_core.u_i2c_fsm
+Fsm state_q "3597806508"
+Transition Active->AcquireStart "1->21"
