eaa8e8d1d2b0c590edf8750b3ecb09a39455daef
Marno van der Maas <mvdmaas+git@lowrisc.org>
2024-03-05 16:55:06 +0000

    [i2c,rtl] Implement NACK after timeout
    This is quite a big commit, but I'm not sure how to split this into
    smaller atomic pieces.
    
    It does the following things:
    - Adds NACK states to the FSM.
    - Add AcqNack to info byte enum and also increase the ACQ FIFO width
    because the enum now takes up 3 bits instead of 2.
    - Add timer to target stretch states so that it can timeout.
    - Connect up the target timeout control register to control whether the
    target sends a NACK after a certain amount of time stretching.
    
    Signed-off-by: Marno van der Maas <mvdmaas+git@lowrisc.org>

--- hw/ip/i2c/rtl/i2c_pkg.sv
+++ hw/ip/i2c/rtl/i2c_pkg.sv
@@ -5,11 +5,18 @@
 // i2c package
 //
 package i2c_pkg;
-  typedef enum logic [1:0] {
-    AcqData = 2'b00,
-    AcqStart = 2'b01,
-    AcqStop = 2'b10,
-    AcqRestart = 2'b11
+
+  // TODO encode this more efficiently in the ACQ FIFO. Each entry in the ACQ
+  // FIFO does not need to contain both an 8 bit data field and a 3 bit
+  // identifier. We should have the ACQ FIFO be 9 bits wide where the MSB
+  // indicates whether it is a data byte or a control byte. This way we can
+  // add more values to this enum without having to widen the ACQ FIFO width.
+  typedef enum logic [2:0] {
+    AcqData      = 3'b000,
+    AcqStart     = 3'b001,
+    AcqStop      = 3'b010,
+    AcqRestart   = 3'b011,
+    AcqNack      = 3'b100
   } i2c_acq_byte_id_e;
 
 endpackage : i2c_pkg
